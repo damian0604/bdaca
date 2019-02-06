07d9bb20e39de4778db2835e69339b30e5ab7119
damian0604 <d.c.trilling@uva.nl>
2019-02-06 16:35:34 +0100

    week 2

---
+++ rm-course-2/week2/gradechecker_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+grades = [4, 7.8, 3.6, 9.1, 4.2, 7, 5.5]
+
+for grade in grades:
+    if grade >= 5.5:
+        print(grade,'is a PASS')
+    else:
+        print(grade,'is a FAIL')
